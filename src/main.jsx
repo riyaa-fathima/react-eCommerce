295633b041c0118d6ded29c1382ef62786c6c97f
riyafathima2005 <riyafathima9889@gmail.com>
2025-09-22 11:59:25 +0530

    navbar file name change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./components/Navbar/Navbar.jsx";
+import NavBar from "./components/Navbar/NavBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
